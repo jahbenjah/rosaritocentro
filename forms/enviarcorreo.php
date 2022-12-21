65d8796ae0d70324bbca1b320418c3b93b4e51cf
MauricioAlcala <a_mauricio@live.com>
2022-12-21 15:13:33 -0800

    reCAPTCHA

--- forms/enviarcorreo.php
+++ forms/enviarcorreo.php
@@ -29,5 +29,21 @@ require("../assets/vendor/PHPMailer/src/SMTP.php");
  } else {
  echo "OK";
  }
+if(isset($_POST['g-recaptcha-response'])) {
+   // RECAPTCHA SETTINGS
+   $captcha = $_POST['g-recaptcha-response'];
+   $ip = $_SERVER['REMOTE_ADDR'];
+   $key = '6Lfwi24gAAAAAN_yYd6f2hBlFDZA7AKHfFli3eWt';
+   $url = 'https://www.google.com/recaptcha/api/siteverify';
 
+   // RECAPTCH RESPONSE
+   $recaptcha_response = file_get_contents($url.'?secret='.$key.'&response='.$captcha.'&remoteip='.$ip);
+   $data = json_decode($recaptcha_response);
+
+   if(isset($data->success) &&  $data->success === true) {
+   }
+   else {
+      die('Your account has been logged as a spammer, you cannot continue!');
+   }
+ }
  ?>
