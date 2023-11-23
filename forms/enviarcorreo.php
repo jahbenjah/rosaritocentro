36ab3eb076a766e214014f6373e2044b85e58fbf
VictorAnaya2023 <148127887+VictorAnaya2023@users.noreply.github.com>
2023-11-23 12:28:10 -0800

    captcha added

--- forms/enviarcorreo.php
+++ forms/enviarcorreo.php
@@ -21,14 +21,51 @@ require("../assets/vendor/PHPMailer/src/SMTP.php");
  $subject = $_POST['subject'];
  $message = $_POST['message'];
 
+ if (!$name || !$email || !$subject || !$message) {
+   echo "Please fill out all the required fields.";
+   error_log("No se proporcionaron todos los campos requeridos");
+   exit;
+}
+
+$recaptcha_secret = '6Lfwi24gAAAAAN_yYd6f2hBlFDZA7AKHfFli3eWt';
+$recaptcha_response = $_POST['token'];
+
+if(!$recaptcha_response){
+   error_log("ocurrio un error al obtener el token de captcha");
+}
+
+$url = 'https://www.google.com/recaptcha/api/siteverify';
+$data = [
+    'secret' => $recaptcha_secret,
+    'response' => $recaptcha_response,
+];
+
+$options = [
+   'http' => [
+       'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+       'method' => 'POST',
+       'content' => http_build_query($data),
+   ],
+];
+
+$context = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+$result_json = json_decode($result, true);
+
+if ($result_json['success']) {
+   $mail->Body = "Nombre: {$name}  <br> Correo Electronico: {$email} <br> Asunto : {$subject} <br> Mensaje : {$message}";
+   $mail->AddAddress("contacto@rosaritocentro.com");
+   if(!$mail->Send()) {
+      echo "Mailer Error: " . $mail->ErrorInfo;
+   } else {
+      echo "OK";
+   }
+}else {
+    error_log("ocurrio un error con el captcha");
+    exit;
+}
  
- $mail->Body = "Nombre: {$name}  <br> Correo Electronico: {$email} <br> Asunto : {$subject} <br> Mensaje : {$message}";
- $mail->AddAddress("contacto@rosaritocentro.com");
- if(!$mail->Send()) {
- echo "Mailer Error: " . $mail->ErrorInfo;
- } else {
- echo "OK";
- }
+ 
 
    // Validate reCAPTCH Response
 /*if(isset($_POST['g-recaptcha-response'])) {
